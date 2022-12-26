9b70b0bd6dcd5a4d08415cb223cab20355895bdd
Alina <beketova2007@bk.ru>
2022-12-26 20:06:05 +0300

    portfolio

---
+++ src/components/btnGitHub/BtnGitHub.js
@@ -0,0 +1,11 @@
+import './styles.css'
+
+const BtnGitHub = ({link}) => {
+    return ( 
+        <a href={link} target="_blank" rel='noreferrer' className="btn-outline">
+            link
+        </a>
+    );
+}
+ 
+export default BtnGitHub;
---
+++ src/helpers/projectsList.js
@@ -0,0 +1,57 @@
+import project02 from "./../img/projects/02.png";
+
+import project03 from "./../img/projects/03.png";
+
+import project04 from "./../img/projects/04.png";
+
+import project05 from "./../img/projects/05.png";
+
+import project06 from "./../img/projects/06.png";
+
+import project01 from "./../img/projects/01.png";
+
+
+const projects = [
+	{
+		title: 'Types of Castiel',
+		skills: 'React, MongoDB, JavaScript, HTML, Css',
+		img: project01,
+		imgBig: project01,
+	},
+	{
+		title: 'Avito',
+		img: project02,
+		imgBig: project02,
+		skills: 'JavaScript, HTML, Css',
+		gitHubLink: 'https://alinabeketova.github.io/avito_advertisement_page/',
+	},
+	{
+		title: 'Guitar shop',
+		img: project03,
+		imgBig: project03,
+		skills: 'JavaScript, HTML, Css',
+		gitHubLink: 'https://alinabeketova.github.io/shop-------------/',
+	},
+	{
+		title: 'Auto shop',
+		img: project04,
+		imgBig: project04,
+		skills: 'React, JavaScript, HTML, Css',
+	},
+	{
+		title: 'Landing',
+		img: project05,
+		imgBig: project05,
+		skills: 'HTML, Css, JavaScript',
+		gitHubLink: 'https://alinabeketova.github.io/to-do-list/',
+	},
+	{
+		title: 'Gaming community',
+		img: project06,
+		imgBig: project06,
+		skills: 'HTML, Css, JavaScript',
+		gitHubLink: 'https://alinabeketova.github.io/Alivio/',
+	},
+];
+
+export {projects}
